ff24387a71e5601e9e7118bc098a1a170506df29
Thibaud Amielh <thibaud.amielh@gmail.com>
2020-02-10 00:22:23 +0300

    Updates

---
+++ precache-manifest.1d1475a64d45bc15d613d8188a351226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db024f2148ba7df43d3886e1376807ee",
+    "url": "/victoriakorolkova/index.html"
+  },
+  {
+    "revision": "58c3574c2fde3990f95f",
+    "url": "/victoriakorolkova/static/css/main.51ad15cc.chunk.css"
+  },
+  {
+    "revision": "75a871986000a5869cdc",
+    "url": "/victoriakorolkova/static/js/2.cc71e8ef.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/victoriakorolkova/static/js/2.cc71e8ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58c3574c2fde3990f95f",
+    "url": "/victoriakorolkova/static/js/main.53e2ed25.chunk.js"
+  },
+  {
+    "revision": "54471d037bdb0efb7662",
+    "url": "/victoriakorolkova/static/js/runtime-main.2305ec42.js"
+  }
+]);
